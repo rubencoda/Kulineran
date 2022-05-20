63908fd422b1201e41ae6ae8112e9f4fb563621e
rubencoda <rubencoda2310@gmail.com>
2022-05-20 22:09:21 +0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="row mt-4">
+		<div class="col">
+			<div class="d-flex h-100">
+				<div class="justify-content-center align-self-center">
+					<h2>
+						<strong>Delicious Food Menu,</strong> <br />
+						In Your Gadget
+					</h2>
+					<p>Ayo segera pilih dan pesan makanan favorite anda</p>
+					<button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right> Pesan</button>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<img src="../assets/images/hero.png" width="100%" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HeroView",
+};
+</script>
+
+<style></style>
---
+++ src/views/Foods.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<Navbar />
+		<h2>Daftar Makanan</h2>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+	name: "FoodsView",
+	components: {
+		Navbar,
+	},
+};
+</script>
+
+<style></style>
