52c4f3f9bd43ae0e9a61bfbaad82040abf44f0cb
rubencoda <rubencoda2310@gmail.com>
2022-05-21 12:41:57 +0700

    fooddetail

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+	<footer class="mt-5 mb-4">
+		<hr />
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">2022 Copyright RUBEN CODA</div>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "FooterView",
+};
+</script>
+
+<style></style>
---
+++ src/views/FoodDetail.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="food-detail">
+		<Navbar />
+		<div class="container">
+			<!-- Start Breaccrumb -->
+			<div class="row mt-5">
+				<div class="col">
+					<nav aria-label="breadcrumb ">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item"><router-link to="/" class="text-dark">Home</router-link></li>
+							<li class="breadcrumb-item"><router-link to="/foods" class="text-dark">Food Order </router-link></li>
+							<li class="breadcrumb-item active">Food Order</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+			<!-- End Breaccrumb -->
+
+			<!-- Start Food Detail -->
+			<div class="row mt-3">
+				<div class="col-md-6">
+					<img :src="'../assets/images/' + product.gambar" class="img-fluid shadow" />
+				</div>
+				<div class="col-md-6">
+					<h2>
+						<strong>{{ product.nama }}</strong>
+					</h2>
+					<hr />
+					<h4>
+						Harga <strong>{{ product.harga }}</strong>
+					</h4>
+					<form action="" class="mt-4">
+						<div class="form-group">
+							<label for="jumlah_pemesanan">Jumlah Pesan</label>
+							<input type="number" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label for="keterangan">Keterangan</label>
+							<textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Keterangan spt : pedas"></textarea>
+						</div>
+						<button type="submit" class="btn btn-success"><b-icon-cart></b-icon-cart> Pesan</button>
+					</form>
+				</div>
+			</div>
+			<!-- End Food Detail -->
+		</div>
+	</div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
+
+export default {
+	name: "FoodsDetail",
+	components: {
+		Navbar,
+	},
+	data() {
+		return {
+			product: [],
+		};
+	},
+	methods: {
+		setProduct(data) {
+			this.product = data;
+		},
+	},
+	mounted() {
+		axios
+			.get("http://localhost:3000/products/" + this.$route.params.id)
+			.then((response) => this.setProduct(response.data))
+			.catch((error) => console.log("Gagal : ", error));
+	},
+};
+</script>
+
+<style></style>
